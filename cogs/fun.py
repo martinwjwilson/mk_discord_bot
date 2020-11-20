7a8b5f2fb5a912438fb06237af9a838061c5ee30
Martin <martinwjwilson@gmail.com>
2020-11-20 16:48:22 +0000

    converted the previous changes into a method

--- cogs/fun.py
+++ cogs/fun.py
@@ -15,38 +15,31 @@ class Fun(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'pong')
 
-    # async def testing(self, number):
-    #     return number * 2
+    async def get_track_rows(self, sheet_data, track_name):
+        """
+        Returns a list containing records of the requested track
+        """
+        required = False # are the current rows required
+        current_row = 1 # track the number of the current row
+        track_times_list = []
+        # loop through each row and return the details of the requested track
+        for row in sheet_data:
+            if required and row[0] == '':
+                required = False
+            if track_name in row:
+                required = True
+            if required:
+                track_times_list.append(row)
+            current_row += 1
+        return track_times_list
 
     @commands.command()
     async def track(self, ctx, *, track_name: str):
         # Retrieve the track data from the sheet
-        # print(await self.testing(track_name))
-        res = worksheet.get_all_values()
-        # res = worksheet.row_values(4)
-
-
-        # show the row containing the track name
-        # only print the rows after that
-        # stop printing when the first cell is blank
-        found = False
-        i = 1
-        for row in res:
-            if found and row[0] == '':
-                print("stop here")
-            if track_name in row:
-                found = True
-                print(str(i))
-            if found:
-                print(row)
-            i += 1
-        # keep printing numbers until the first cell contains 'Best time:'
-
-
-
-
-        # Create a dictionary of scores
-        # Create an instance of a track
+        sheet_data = worksheet.get_all_values()
+        await self.get_track_rows(sheet_data, track_name)
+        # Create a dictionary of users and their scores for the specific track
+        # Create an instance of a track using a dictionary of users and scores
 
 
 def setup(bot):
