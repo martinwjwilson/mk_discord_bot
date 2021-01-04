90bad26282ec4f3dd8804946c28aa914aed9c986
Martin <martinwjwilson@gmail.com>
2021-01-04 20:45:50 +0000

    moved ping command to general

--- cogs/track.py
+++ cogs/track.py
@@ -11,14 +11,6 @@ class Track(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(help = "This is the longer description of the help for this command", brief = "This is the brief text description")
-    # @commands.has_role(config.role_dict.get("admin"))
-    async def ping(self, ctx, num: int):
-        # testing = Player("Martin", 1, 10, 100)
-        # await ctx.send(testing.name)
-        x = lambda a : a + 10
-        await ctx.send(x(num))
-
     async def get_track_rows(self, sheet_data, track_name: str) -> list:
         """
         Returns a list of rows containing records of the requested track
